3f520ed4e7f5d1b594cd92ccda304aaf708d4deb
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2025-11-04 16:14:47 +0530

    Time: 1 ms (86.41%), Space: 47.2 MB (77.32%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,26 +1,66 @@
+// class Solution {
+//     public int findCircleNum(int[][] isConnected) {
+//         int n= isConnected.length;
+//         int count=0;
+
+//         boolean[] vis= new boolean[n];
+
+//         for(int i=0;i<n;i++){
+//             if(!vis[i]){
+//                 dfs(isConnected,vis,i);
+//                 count++;
+//             }
+//         }
+
+//         return count;
+        
+//     }
+
+//     private void dfs(int[][] isConnected,boolean[] visited, int city){
+//         visited[city]= true;
+
+//         int m= isConnected.length;
+//         for(int j=0;j<m;j++){
+//             if(isConnected[city][j]==1 && !visited[j]){
+//                 dfs(isConnected,visited,j);
+//             }
+//         }
+//     }
+// }
+
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         int n= isConnected.length;
+        boolean[] vis= new boolean[n];
         int count=0;
-        boolean[] vis = new boolean[n];
+
         for(int i=0;i<n;i++){
             if(!vis[i]){
-                dfs(isConnected, vis,i);
+                bfs(isConnected,vis,i);
+
                 count++;
             }
         }
 
         return count;
-        
     }
 
-    private void dfs(int[][] isConnected, boolean[] visited, int city){
-        visited[city]= true;
-        
-        int m= isConnected.length;
-        for(int j=0;j<m;j++){
-            if(isConnected[city][j]==1 && !visited[j]){
-                dfs(isConnected, visited, j);
+
+    private void bfs(int[][] isConnected, boolean[] visited, int start){
+        Queue<Integer> q= new LinkedList<>();
+        q.add(start);
+        visited[start]= true;
+
+        while(!q.isEmpty()){
+            int city= q.poll();
+            int m= isConnected.length;
+
+            for(int j=0;j<m;j++){
+                if(isConnected[city][j]==1 && !visited[j]){
+                    visited[j]= true;
+
+                    q.add(j);
+                }
             }
         }
     }
