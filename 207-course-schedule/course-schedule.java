f750af50d7ca181c6a9dd0b8289c464c295072b9
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2025-11-09 19:03:50 +0530

    Time: 19 ms (6.68%) | Memory: 46.9 MB (10.58%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,42 +1,41 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> graph = new ArrayList<>();
-        int indegree[] = new int[numCourses];
-
+        List<List<Integer>> graph= new LinkedList<>();
         for(int i=0;i<numCourses;i++){
-            graph.add(new ArrayList<>()); 
-        }
-
-        for(int[] pair: prerequisites){
-            int a= pair[0];
-            int b= pair[1];
-            graph.get(b).add(a);
-            indegree[a]++;
-        }
-
-            Queue<Integer> q= new LinkedList<>();
-            for(int i=0;i<numCourses;i++){
-                if(indegree[i]==0){
-                q.offer(i);
-            }
-        }
-
-            int completedCourses=0;
-            while(!q.isEmpty()){
-                int course= q.poll();
-                completedCourses++;
-
-                for(int neighbour: graph.get(course)){
-                    indegree[neighbour]--;
-
-                    if(indegree[neighbour]==0){
-                        q.offer(neighbour);
-                    }
-
-                }
-            }
-
-            return completedCourses==numCourses;
-        }
-        
+        graph.add(new ArrayList<>());
     }
+
+    int[] indegree= new int[numCourses];
+
+
+    for(int[] pair: prerequisites){
+        int course= pair[0];
+        int prereq= pair[1];
+        graph.get(prereq).add(course);
+        indegree[course]++;
+    }
+
+    Queue<Integer> q= new LinkedList<>();
+    for(int i=0;i<numCourses;i++){
+        if(indegree[i]==0){
+            q.offer(i);
+        }
+    }
+
+    int count=0;
+    while(!q.isEmpty()){
+        int course= q.poll();
+        count++;
+
+        for(int neighbour: graph.get(course)){
+            indegree[neighbour]--;
+
+            if(indegree[neighbour]==0){
+                q.offer(neighbour);
+            }
+        }
+    }
+
+    return count==numCourses;
+}
+}
