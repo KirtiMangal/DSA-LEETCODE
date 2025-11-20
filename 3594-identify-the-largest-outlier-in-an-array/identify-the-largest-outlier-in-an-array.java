75db93a569f41693fdb3903ce4ca6e19d074565a
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2025-11-20 14:38:07 +0530

    Time: 123 ms (24.87%) | Memory: 106.2 MB (29.73%) - LeetSync

--- 3594-identify-the-largest-outlier-in-an-array/identify-the-largest-outlier-in-an-array.java
+++ 3594-identify-the-largest-outlier-in-an-array/identify-the-largest-outlier-in-an-array.java
@@ -1,47 +1,60 @@
 class Solution {
-    public boolean binarySearch(int[] nums, double target) {
-        int left = 0;
-        int right = nums.length - 1;
+    public boolean binarySearch(int[] nums,int target){
+        int n= nums.length;
+        int left=0;
+        int right=n-1;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+        while(left<=right){
+            int mid= left+(right-left)/2;
 
-            if (nums[mid] == target) {
+            if(nums[mid]==target){
                 return true;
-            } 
-            else if (nums[mid] < target) {
-                left = mid + 1;
-            } 
-            else {
-                right = mid - 1;
+            }
+
+            else if(nums[mid]<target){
+                left=mid+1;
+            }
+
+            else{
+                right=mid-1;
             }
         }
+
         return false;
     }
 
     public int getLargestOutlier(int[] nums) {
-        Arrays.sort(nums); 
+        Arrays.sort(nums);
+        
+        int totalSum= Arrays.stream(nums).sum();
+        Map<Integer,Integer> map= new HashMap<>();
 
-        int totalSum = Arrays.stream(nums).sum();
-        Map<Integer, Integer> freq = new HashMap<>();
-
-        for (int num : nums) 
-        {
-            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        for(int num:nums){
+            map.put(num, map.getOrDefault(num,0)+1);
         }
 
-        for (int i = nums.length - 1; i >= 0; i--) {
-            double potentialSum = (totalSum - nums[i]) / 2.0;
+        int n= nums.length;
 
-            if (binarySearch(nums, potentialSum)) 
-            {
-                if ((potentialSum == nums[i] && freq.get(nums[i]) >= 2) || potentialSum != nums[i]) 
+        for(int i=n-1; i>=0;i--){
+            double potentialSum= (totalSum-nums[i])/2.0;
+
+             if (potentialSum % 1 != 0) 
+             
+             continue;
+            int target = (int) potentialSum;
+
+            if (binarySearch(nums, target)) {
+
+        if ((potentialSum == nums[i] && map.get(nums[i]) >= 2) 
+                    || potentialSum != nums[i]) 
                 {
                     return nums[i];
                 }
             }
         }
 
-        return 0;
+        return 0; 
     }
 }
+
+           
