6ac48537a36f88fb8833b1ce3a74204c22e9ccf1
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2025-11-27 14:53:43 +0530

    Time: 6 ms (99.22%) | Memory: 60.6 MB (32.04%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -1,28 +1,27 @@
 class Solution {
     public int numEnclaves(int[][] grid) {
-        int m= grid.length;
-        int n= grid[0].length;
+        int n= grid.length;
+        int m= grid[0].length;
 
-        for(int i=0;i<m;i++){
+        for(int i=0;i<n;i++){
             if(grid[i][0]==1)
             dfs(grid,i,0);
 
-            if(grid[i][n-1]==1)
-            dfs(grid,i,n-1);
-
+            if(grid[i][m-1]==1)
+            dfs(grid,i,m-1);
         }
 
-        for(int j=0;j<n;j++){
+        for(int j=0;j<m;j++){
             if(grid[0][j]==1)
             dfs(grid,0,j);
 
-            if(grid[m-1][j]==1)
-            dfs(grid,m-1,j);
+            if(grid[n-1][j]==1)
+            dfs(grid,n-1,j);
         }
 
         int count=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(grid[i][j]==1)
                 count++;
             }
@@ -32,20 +31,21 @@ class Solution {
         
     }
 
-
 public void dfs(int[][] grid,int i,int j){
-    int m= grid.length;
-    int n= grid[0].length;
+    int n= grid.length;
+    int m= grid[0].length;
 
-    if(i<0 || i>=m || j<0 || j>=n || grid[i][j]!=1){
+    if(i<0 || i>=n || j<0 || j>=m || grid[i][j]!=1){
         return;
     }
 
     grid[i][j]=0;
 
     dfs(grid,i+1,j);
-    dfs(grid,i-1,j);
     dfs(grid,i,j+1);
+    dfs(grid,i-1,j);
     dfs(grid,i,j-1);
+
 }
+
 }
