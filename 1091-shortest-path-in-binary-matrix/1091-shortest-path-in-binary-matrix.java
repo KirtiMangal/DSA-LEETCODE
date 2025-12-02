ed083c8a1e6138e1501807cc195fe27995c1f9a9
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2025-12-02 15:17:51 +0530

    Time: 15 ms (68.85%), Space: 47.5 MB (36.03%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -4,27 +4,23 @@ class Pair{
     int col;
 
     public Pair(int dist,int row,int col){
-        this.dist= dist;
-        this.row= row;
+        this.dist=dist;
+        this.row=row;
         this.col=col;
     }
 }
-
-
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
         int n= grid.length;
         int m= grid[0].length;
+        int source[]= {0,0};
+        int destination[]={n-1,m-1};
 
-        int[] source={0,0};
-        int[] destination={n-1,m-1};
-
-        
         if(grid[source[0]][source[1]]!=0 || grid[destination[0]][destination[1]]!=0){
             return -1;
         }
 
-         if(destination[0] == source[0] && destination[1]==source[1]){
+        if(destination[0]== source[0] && destination[1]==source[1]){
             return 1;
         }
 
@@ -49,21 +45,21 @@ class Solution {
             int c= it.col;
 
             for(int i=0;i<8;i++){
-                int newR= r+dR[i];
-                int newC= c+dC[i];
+            int newR= r+dR[i];
+            int newC= c+dC[i];
 
-                if(newR>=0 && newR<n && newC>=0 && newC<m && grid[newR][newC]==0 && dis+1<dist[newR][newC]){
-                    dist[newR][newC]= dis+1;
+            if(newR>=0 && newR<n && newC>=0 && newC<m && grid[newR][newC]==0 && dis+1<dist[newR][newC]){
+                dist[newR][newC]= dis+1;
 
-                    if(newR==destination[0] && newC==destination[1]){
-                        return dis+1;
-                    }
-
-                    q.add(new Pair(dis+1,newR,newC));
+                if(newR==destination[0] && newC==destination[1]){
+                    return dis+1;
                 }
+                q.add(new Pair(dis+1,newR,newC));
+
             }
         }
-        return -1;
-        
     }
+
+    return -1;
+}
 }
