e575ab4b31df5ddf3101beb854060570c872a736
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2025-12-13 02:32:44 +0530

    Time: 3 ms (86.55%) | Memory: 48 MB (7.74%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,66 +1,36 @@
-// class Solution {
-//     public int candy(int[] ratings) {
-//           int n = ratings.length;
-//         if (n == 0) return 0;
-
-//         int sum = 1;
-//         int up = 1;     
-//         int down = 0;  
-//         int peak = 1;    
-//         for (int i = 1; i < n; i++) {
-//             if (ratings[i] > ratings[i - 1]) {
-//                 up++;
-//                 peak = up;
-//                 down = 0;
-//                 sum += up;
-//             } else if (ratings[i] == ratings[i - 1]) {
-//                 up = peak = 1;
-//                 down = 0;
-//                 sum += 1;
-//             } else {  
-//                 down++;
-//                 up = 1;
-//                 sum += down;
-//                 if (down >= peak) {
-//                     sum += 1;
-//                 }
-//             }
-//         }
-//         return sum;
-//     }}
-
 class Solution {
     public int candy(int[] ratings) {
-        int n = ratings.length;
-        if (n == 0) 
+        int n= ratings.length;
+        if(n==0)
         return 0;
-        
-        int sum=1,i=1;
+
+        int sum=1;
+        int i=1;
         while(i<n){
-            while(i < n && ratings[i]==ratings[i-1]){
+            while(i<n && ratings[i]==ratings[i-1]){
                 sum+=1;
                 i++;
-              
             }
 
-           int  peak=1;
+            int peak=1;
             while(i<n && ratings[i]>ratings[i-1]){
                 peak+=1;
-                 sum+=peak;
-                 i++;
-                 
-
-            }
-           int down = 1;
-            while (i < n && ratings[i] < ratings[i - 1]) {
-                sum += down;
-                 down++;
+                sum+=peak;
                 i++;
             }
-            if(down>peak){
-                sum=sum+(down-peak);
+
+            int down=1;
+            while(i<n && ratings[i]<ratings[i-1]){
+                sum+=down;
+                down++;
+                i++;
             }
-    
+
+            if(down>peak){
+                sum+=(down-peak);
+            }
         }
-         return sum;
-    }}
+
+        return sum;
+    }
+}
