1252a1823fb059cfa65f6be3265cddf239a67355
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2025-12-22 00:08:31 +0530

    Time: 8 ms (74.88%) | Memory: 47.1 MB (25.44%) - LeetSync

--- 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.java
+++ 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.java
@@ -4,12 +4,11 @@ class Solution {
         int cols = strs[0].length();
         int count = 0;
 
-        // column by column check
         for (int col = 0; col < cols; col++) {
             for (int row = 0; row < rows - 1; row++) {
                 if (strs[row].charAt(col) > strs[row + 1].charAt(col)) {
-                    count++;      // this column is not sorted
-                    break;        // move to next column
+                    count++;      
+                    break;        
                 }
             }
         }
