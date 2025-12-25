fd6f04f1f549d1827164f4eec3c3ecf2a205b55c
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2025-12-25 11:08:54 +0530

    Time: 1 ms (91.74%) | Memory: 45.1 MB (15.52%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.java
+++ 541-reverse-string-ii/reverse-string-ii.java
@@ -1,38 +1,63 @@
+// class Solution {
+//     public String reverseStr(String s, int k) {
+
+//     char[] arr = s.toCharArray();
+//     int n = arr.length;
+//     int i = 0;
+//     while(i<n){
+//         int rem= n-i;
+
+//         if(rem>=k){
+//             int left=i;
+//             int right= i+k-1;
+
+//             while(left<right){
+//             char temp=arr[left];
+//             arr[left]= arr[right];
+//             arr[right]= temp;
+
+//             left++;
+//             right--;
+//         }
+
+//         if(rem>=2*k){
+//             i+=2*k;
+//         }
+
+//         else{
+//             break;
+//         }
+
+//         }
+
+//         else{
+//             int left=i;
+//             int right= n-1;
+
+//             while(left<right){
+//                 char temp= arr[left];
+//                 arr[left]= arr[right];
+//                 arr[right]= temp;
+
+//                 left++;
+//                 right--;
+//             }
+
+//             break;
+//         }
+//     }
+//     return new String(arr);
+//     }
+// }
+
 class Solution {
     public String reverseStr(String s, int k) {
+        char[] arr= s.toCharArray();
+        int n= s.length();
 
-    char[] arr = s.toCharArray();
-    int n = arr.length;
-    int i = 0;
-    while(i<n){
-        int rem= n-i;
-
-        if(rem>=k){
+        for(int i=0;i<n;i+=2*k){
             int left=i;
-            int right= i+k-1;
-
-            while(left<right){
-            char temp=arr[left];
-            arr[left]= arr[right];
-            arr[right]= temp;
-
-            left++;
-            right--;
-        }
-
-        if(rem>=2*k){
-            i+=2*k;
-        }
-
-        else{
-            break;
-        }
-
-        }
-
-        else{
-            int left=i;
-            int right= n-1;
+            int right= Math.min(i+k-1, n-1);
 
             while(left<right){
                 char temp= arr[left];
@@ -42,10 +67,7 @@ class Solution {
                 left++;
                 right--;
             }
-
-            break;
         }
-    }
-    return new String(arr);
-    }
-}
+
+        return new String(arr);
+    }}
