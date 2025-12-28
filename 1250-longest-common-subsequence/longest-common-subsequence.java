8670104cb8b48d477d89e53b652bf83cb2d0ce4c
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2025-12-28 20:09:04 +0530

    Time: 20 ms (77.65%) | Memory: 54.2 MB (9.79%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -4,7 +4,6 @@ class Solution {
         int m= text2.length();
 
         int[][] dp= new int[n+1][m+1];
-
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 if(text1.charAt(i-1)==text2.charAt(j-1)){
@@ -12,7 +11,7 @@ class Solution {
                 }
 
                 else{
-                    dp[i][j]= Math.max(dp[i-1][j], dp[i][j-1]);
+                    dp[i][j]= Math.max(dp[i][j-1],dp[i-1][j]);
                 }
             }
         }
