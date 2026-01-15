2ce721705d38e0590ce05f8b8efa303f734787f2
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-01-15 17:09:23 +0530

    Time: 5 ms (17.09%), Space: 43.6 MB (19.58%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,10 +1,10 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st= new Stack<>();
         int n= s.length();
+        Stack<Character> st= new Stack<>();
 
         for(int i=0;i<n;i++){
-            if(s.charAt(i)=='(' || s.charAt(i)=='{' | s.charAt(i)=='['){
+            if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='['){
                 st.push(s.charAt(i));
             }
 
@@ -14,14 +14,14 @@ class Solution {
                 }
 
                 if((st.peek()=='(' && s.charAt(i)==')' ||
-                   st.peek()=='{' && s.charAt(i)=='}' ||
-                   st.peek()=='[' && s.charAt(i)==']')){
-                    st.pop();
-                   }
+                    st.peek()=='{' && s.charAt(i)=='}' ||
+                    st.peek()=='[' && s.charAt(i)==']')){
+                        st.pop();
+                    }
 
-                   else{
-                    return false;
-                   }
+                    else{
+                        return false;
+                    }
             }
 
 
