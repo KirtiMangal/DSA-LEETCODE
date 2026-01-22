240b70c7fe149a39d84cb691e1841906a88ea371
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-01-22 22:36:36 +0530

    Time: 3 ms (66.06%) | Memory: 44.5 MB (62.66%) - LeetSync

---
+++ 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int minimumPairRemoval(int[] nums) {
+        List<Integer> arr = new ArrayList<>();
+        for (int x : nums) arr.add(x);
+
+        int operations = 0;
+
+        while (!isSorted(arr)) {
+            int minIndex = 0;
+
+            for (int i = 0; i < arr.size() - 1; i++) {
+                if (arr.get(i) + arr.get(i + 1) <
+                    arr.get(minIndex) + arr.get(minIndex + 1)) {
+                    minIndex = i;
+                }
+            }
+
+            arr.set(minIndex, arr.get(minIndex) + arr.get(minIndex + 1));
+            arr.remove(minIndex + 1);
+
+            operations++;
+        }
+
+        return operations;
+    }
+
+    private boolean isSorted(List<Integer> arr) {
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr.get(i) < arr.get(i - 1)) 
+            return false;
+        }
+        return true;
+    }
+}
