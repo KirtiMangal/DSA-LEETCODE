4dcbf9feee731374e48f6734fd7cb530e2ff5169
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-01-24 21:04:21 +0530

    Time: 18 ms (80.54%), Space: 97.4 MB (98.44%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -6,16 +6,17 @@ class Solution {
         return res;
     }
 
-    void dfs(int start, int n, int k, List<Integer> cur) {
-        if (cur.size() == k) {
-            res.add(new ArrayList<>(cur));
+    void dfs(int start, int n, int k, List<Integer> curr) {
+        if (curr.size() == k) {
+            res.add(new ArrayList<>(curr));
             return;
         }
 
         for (int i = start; i <= n; i++) {
-            cur.add(i);
-            dfs(i + 1, n, k, cur);
-            cur.remove(cur.size() - 1);
+            curr.add(i);
+
+            dfs(i + 1, n, k, curr);
+            curr.remove(curr.size() - 1);
         }
     }
 }
