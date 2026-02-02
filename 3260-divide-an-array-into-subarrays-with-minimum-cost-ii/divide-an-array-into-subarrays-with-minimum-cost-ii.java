88bd3f6117fef2dfb724a1b64bb6a241ae6ecc81
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-02-02 22:40:10 +0530

    Time: 382 ms (26.09%) | Memory: 103.5 MB (26.09%) - LeetSync

--- 3260-divide-an-array-into-subarrays-with-minimum-cost-ii/divide-an-array-into-subarrays-with-minimum-cost-ii.java
+++ 3260-divide-an-array-into-subarrays-with-minimum-cost-ii/divide-an-array-into-subarrays-with-minimum-cost-ii.java
@@ -1,16 +1,12 @@
 class Solution {
     public long minimumCost(int[] nums, int k, int dist) {
-        // O(n)
+        
         TreeSet<Integer> currset = new TreeSet<>((a,b)-> nums[a]!=nums[b] ? nums[a]-nums[b] : a-b);
         TreeSet<Integer> futset = new TreeSet<>((a,b)-> nums[a]!=nums[b] ? nums[a]-nums[b] : a-b);
         long cost = Long.MAX_VALUE;
 
         int n = nums.length;
-        long currsum = 0l; // sum of cost of all subarays in a wind
-
-        // populate currset and futset
-        // currser = top k-1 min elements
-        // futset = rem elements
+        long currsum = 0l;
 
         for(int i=1; i<=dist+1; i++) {
             currset.add(i);
@@ -24,30 +20,34 @@ class Solution {
         }
 
         cost = currsum;
-        // O(n*logn)
+
         for(int i=1; i < n-1-dist; i++) {
             int rightInd = i+dist+1;
             int leftInd = i;
+
             if(currset.contains(leftInd)) {
                 currsum -= nums[leftInd];
                 currset.remove(leftInd);
                 currsum += nums[rightInd];
                 currset.add(rightInd);
             }
+
             else {
                 futset.remove(leftInd);
                 futset.add(rightInd);
             }
 
-            // balancing
             if(futset.size() > 0) {
                 int minF = futset.first();
                 int maxC = currset.last();
+
                 if(nums[minF] < nums[maxC]) {
                     currsum -= nums[maxC];
+                    
                     currset.remove(maxC);
                     currset.add(minF);
                     currsum += nums[minF];
+
                     futset.remove(minF);
                     futset.add(maxC);
                 }
