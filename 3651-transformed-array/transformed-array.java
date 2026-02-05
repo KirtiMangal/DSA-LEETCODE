6126cc01e34f0cb4eb80fb1e871321850880c1f6
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-02-06 02:16:48 +0530

    Time: 1 ms (98.74%) | Memory: 47 MB (13.84%) - LeetSync

--- 3651-transformed-array/transformed-array.java
+++ 3651-transformed-array/transformed-array.java
@@ -1,6 +1,6 @@
 class Solution {
-    public int[] constructTransformedArray(int[] nums) {
-        int n = nums.length;
+public int[] constructTransformedArray(int[] nums) {       
+     int n = nums.length;
         int[] result = new int[n];
 
         for (int i = 0; i < n; i++) {
