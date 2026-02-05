e1bb44fd5e31cafba6224b3ab9852a1a869848bc
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-02-06 02:15:24 +0530

    Time: 1 ms (98.74%) | Memory: 46.6 MB (86.48%) - LeetSync

---
+++ 3651-transformed-array/transformed-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] constructTransformedArray(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++) {
+
+            int newIndex = (i + nums[i]) % n;
+
+            if (newIndex < 0) {
+                newIndex += n;   // fix negative index
+            }
+
+            result[i] = nums[newIndex];
+        }
+
+        return result;
+    }
+}
