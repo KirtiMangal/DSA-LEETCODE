2e650086629470cac5fb33dc358f68b803cfbe7e
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-02-18 20:55:44 +0530

    Time: 5 ms (66.02%) | Memory: 47.5 MB (16.44%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -2,14 +2,15 @@ class Solution {
     public int minDistance(String word1, String word2) {
         int n= word1.length();
         int m= word2.length();
+
         int[][] dp= new int[n+1][m+1];
 
         for(int i=0;i<=n;i++){
-            dp[i][0]= i;
+            dp[i][0]=i;
         }
 
         for(int j=0;j<=m;j++){
-            dp[0][j]= j;
+            dp[0][j]=j;
         }
 
         for(int i=1;i<=n;i++){
