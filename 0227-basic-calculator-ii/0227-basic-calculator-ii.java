29accb43b39e3a54f1b7adfe9983f942b25c2b32
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-02-18 20:17:29 +0530

    Time: 23 ms (49.63%), Space: 48.5 MB (21.04%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -1,47 +1,51 @@
 class Solution {
     public int calculate(String s) {
         int n= s.length();
-
         if(s==null || n==0){
             return 0;
         }
 
-        int lastNum=0;
+     Stack<Integer> st= new Stack<>();
+
         int currNum=0;
-        char operator= '+';
-        int result=0;
+        char operator='+';
 
         for(int i=0;i<n;i++){
             char ch= s.charAt(i);
-
+        
         if(Character.isDigit(ch)){
-            currNum= currNum*10 +(ch-'0');
+            currNum= currNum*10 + (ch-'0');
         }
 
-        if((!Character.isDigit(ch) && ch!=' ')|| i==n-1){
-            
+        if((!Character.isDigit(ch) && ch!=' ') || i==n-1){
+
         if(operator=='+'){
-            result+= lastNum;
-            lastNum= currNum;
+            st.push(currNum);
         }
 
         else if(operator=='-'){
-            result+= lastNum;
-            lastNum= -currNum;
+            st.push(-currNum);
         }
 
         else if(operator=='*'){
-            lastNum*= currNum;
+            st.push(st.pop()*currNum);
         }
 
         else if(operator=='/'){
-            lastNum/= currNum;
+            st.push(st.pop()/currNum);
         }
 
         operator= ch;
         currNum=0;
-        
-    }}
+    }
+}
 
-return result + lastNum;
-}}
+int result=0;
+
+while(!st.isEmpty()){
+    result+= st.pop();
+}
+
+return result;
+}
+}
