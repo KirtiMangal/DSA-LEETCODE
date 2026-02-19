a1d9215d9df81bbc367bfc83b56de9f5ec87ceb6
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-02-19 10:33:45 +0530

    Time: 1 ms (88.27%) | Memory: 47.5 MB (45.46%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,33 +1,3 @@
-// class Solution {
-//     public int findCircleNum(int[][] isConnected) {
-//         int n= isConnected.length;
-//         int count=0;
-
-//         boolean[] vis= new boolean[n];
-
-//         for(int i=0;i<n;i++){
-//             if(!vis[i]){
-//                 dfs(isConnected,vis,i);
-//                 count++;
-//             }
-//         }
-
-//         return count;
-        
-//     }
-
-//     private void dfs(int[][] isConnected,boolean[] visited, int city){
-//         visited[city]= true;
-
-//         int m= isConnected.length;
-//         for(int j=0;j<m;j++){
-//             if(isConnected[city][j]==1 && !visited[j]){
-//                 dfs(isConnected,visited,j);
-//             }
-//         }
-//     }
-// }
-
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         int n= isConnected.length;
@@ -36,8 +6,7 @@ class Solution {
 
         for(int i=0;i<n;i++){
             if(!vis[i]){
-                bfs(isConnected,vis,i);
-
+                dfs(isConnected,vis,i);
                 count++;
             }
         }
@@ -45,22 +14,13 @@ class Solution {
         return count;
     }
 
+    public static void dfs(int[][] isConnected, boolean[] vis, int city){
+        int m= isConnected.length;
+        vis[city]= true;
 
-    private void bfs(int[][] isConnected, boolean[] visited, int start){
-        Queue<Integer> q= new LinkedList<>();
-        q.add(start);
-        visited[start]= true;
-
-        while(!q.isEmpty()){
-            int city= q.poll();
-            int m= isConnected.length;
-
-            for(int j=0;j<m;j++){
-                if(isConnected[city][j]==1 && !visited[j]){
-                    visited[j]= true;
-
-                    q.add(j);
-                }
+        for(int j=0;j<m;j++){
+            if(isConnected[city][j]==1 && !vis[j]){
+                dfs(isConnected,vis,j);
             }
         }
     }
