894d314a5913032bab4ae7d2007ab6bd64dc4f74
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-02-19 23:29:54 +0530

    Time: 1 ms (99.81%) | Memory: 47.9 MB (37.29%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -4,19 +4,23 @@ class Solution {
         int m= board[0].length;
 
         for(int i=0;i<n;i++){
-            if(board[i][0]=='O')
-            dfs(board,i,0);
+            if(board[i][0]=='O'){
+                dfs(board,i,0);
+            }
 
-            if(board[i][m-1]=='O')
-            dfs(board,i,m-1);
+            if(board[i][m-1]=='O'){
+                dfs(board,i,m-1);
+            }
         }
 
         for(int j=0;j<m;j++){
-            if(board[0][j]=='O')
-            dfs(board,0,j);
+            if(board[0][j]=='O'){
+                dfs(board,0,j);
+            }
 
-            if(board[n-1][j]=='O')
-            dfs(board,n-1,j);
+            if(board[n-1][j]=='O'){
+                dfs(board,n-1,j);
+            }
         }
 
         for(int i=0;i<n;i++){
@@ -32,19 +36,19 @@ class Solution {
         }
     }
 
-        public void dfs(char[][] board,int i, int j){
-            int n= board.length;
-            int m= board[0].length;
+    public static void dfs(char[][] board,int i,int j){
+        int n= board.length;
+        int m= board[0].length;
 
-            if(i<0 || i>=n || j<0 || j>=m || board[i][j]!='O'){
-                return;
-            }
-
-            board[i][j]='T';
-
-            dfs(board,i+1,j);
-            dfs(board,i-1,j);
-            dfs(board,i,j+1);
-            dfs(board,i,j-1);
+        if(i<0 || i>=n || j<0 || j>=m || board[i][j]!='O'){
+            return;
         }
+
+        board[i][j]='T';
+
+        dfs(board,i+1,j);
+        dfs(board,i-1,j);
+        dfs(board,i,j+1);
+        dfs(board,i,j-1);
     }
+}
