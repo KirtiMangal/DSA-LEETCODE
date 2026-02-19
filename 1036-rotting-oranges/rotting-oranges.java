672c6c2492580637bf007d4dd2ae72928111711c
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-02-19 11:11:36 +0530

    Time: 2 ms (91.12%) | Memory: 45.2 MB (5.51%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -2,42 +2,42 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int n= grid.length;
         int m= grid[0].length;
-        int freshCount=0;
+        int count=0;
 
         Queue<int[]> q= new LinkedList<>();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==1){
-                    freshCount++;
+                    count++;
                 }
 
                 else if(grid[i][j]==2){
-                    q.add(new int[] {i,j});
+                    q.add(new int[]{i,j});
                 }
             }
         }
 
-        if(freshCount==0){
-            return 0;
-        }
+                if(count==0){
+                    return 0;
+                }
 
         int time=0;
         while(!q.isEmpty()){
             int size= q.size();
-
+            
             for(int i=0;i<size;i++){
-            int[] rottenLoc= q.poll();
-            int r= rottenLoc[0];
-            int c= rottenLoc[1];
+                int[] rottenLoc= q.poll();
+                int r= rottenLoc[0];
+                int c= rottenLoc[1];
 
             int[][] neighbours= {
+                {r-1,c},
                 {r+1,c},
                 {r,c+1},
-                {r-1,c},
                 {r,c-1}
             };
 
-            for(int[] neighbour: neighbours){
+            for(int neighbour[]: neighbours){
                 int nr= neighbour[0];
                 int nc= neighbour[1];
 
@@ -51,19 +51,17 @@ class Solution {
 
                 q.add(new int[]{nr,nc});
                 grid[nr][nc]=2;
+                count--;
 
-                freshCount--;
-
-                if(freshCount==0)
-                return time+1;
+                if(count==0){
+                    return time+1;
+                }
             }
         }
 
         time++;
-        
     }
 
     return -1;
 }
-
 }
