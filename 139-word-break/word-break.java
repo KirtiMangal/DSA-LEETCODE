b03cdff0737c14322fdcbd5d1698a6ba1cefe9ab
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-02-22 16:34:52 +0530

    Time: 8 ms (45.22%) | Memory: 46.5 MB (19.96%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -6,7 +6,7 @@ class Solution {
         boolean[] dp= new boolean[n+1];
         dp[0]= true;
 
-        for(int i=0;i<=n;i++){
+        for(int i=1;i<=n;i++){
             for(int j=0;j<i;j++){
                 if(dp[j] && set.contains(s.substring(j,i))){
                 dp[i]= true;
