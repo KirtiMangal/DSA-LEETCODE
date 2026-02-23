2cddcec89c7da841403f4bffd9a5a5739ef3a658
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-02-23 16:58:19 +0530

    Time: 8 ms (30.65%) | Memory: 47.2 MB (65.62%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,39 +1,30 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        
         int n= matrix.length;
         int m= matrix[0].length;
 
         int[][] dp= new int[n][m];
-
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(i==0){
                     dp[i][j]= matrix[i][j];
                 }
 
-                // else if(i==0){
-                //     dp[i][j]= matrix[i][j] + matrix[i][j-1];
-                // }
-
-                // else if(j==0){
-                //     dp[i][j]= matrix[i][j] + dp[i-1][j];
-                // }
-
                 else{
-                    int left= (j>0)? dp[i-1][j-1] : Integer.MAX_VALUE;
-                    int right= (j<m-1)? dp[i-1][j+1] : Integer.MAX_VALUE;
+                    int left= (j>0)?dp[i-1][j-1] : Integer.MAX_VALUE;
+                    int right= (j<m-1)?dp[i-1][j+1] : Integer.MAX_VALUE;
 
-                    dp[i][j]= matrix[i][j]+ Math.min(dp[i-1][j], Math.min(left,right));
+                    dp[i][j]= matrix[i][j] + Math.min(dp[i-1][j], Math.min(left,right));
                 }
             }
         }
+
         int ans= Integer.MAX_VALUE;
-        for(int j=0;j<m;j++){
-            ans= Math.min(ans, dp[n-1][j]);
+
+        for(int i=0;i<m;i++){
+            ans= Math.min(ans, dp[n-1][i]);
         }
 
         return ans;
-        
-            }
+    }
 }
