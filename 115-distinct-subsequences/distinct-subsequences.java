44c1039125cf1afbef9a2e8ea2a460e946224f7d
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-02-24 11:39:09 +0530

    Time: 21 ms (34.04%) | Memory: 54.3 MB (72.84%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -9,14 +9,18 @@ class Solution {
             dp[i][0]=1;
         }
 
+        //in distinct, j=1...
+
         for(int j=1;j<=m;j++){
             dp[0][j]=0;
         }
 
+
+
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 if(s.charAt(i-1)==t.charAt(j-1)){
-                    dp[i][j]= dp[i-1][j] + dp[i-1][j-1];
+                    dp[i][j]= dp[i-1][j-1] + dp[i-1][j];
                 }
 
                 else{
@@ -26,6 +30,5 @@ class Solution {
         }
 
         return dp[n][m];
-        
     }
 }
