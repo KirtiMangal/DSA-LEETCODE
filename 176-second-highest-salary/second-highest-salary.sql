e5e6d567773042052a5d9fa7b7ebd552cc1a87b9
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-03-12 10:32:14 +0530

    Time: 259 ms (72.26%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,12 +1,15 @@
 -- # Write your MySQL query statement below
--- SELECT DISTINCT salary AS SecondHighestSalary
--- FROM Employee
--- ORDER BY salary DESC
--- LIMIT 1 OFFSET 1;
+-- Inner query finds the second highest salary.
 
-SELECT (
-    SELECT DISTINCT salary
-    FROM Employee
-    ORDER BY salary DESC
-    LIMIT 1 OFFSET 1
-) AS SecondHighestSalary;
+-- If it does not exist → MySQL returns NULL.
+
+-- Outer SELECT ensures one row is always returned.
+
+SELECT(
+SELECT DISTINCT salary 
+FROM Employee
+ORDER BY salary DESC
+LIMIT 1 OFFSET 1
+)
+AS 
+SecondHighestSalary;
