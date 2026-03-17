a6a18a35ff10370941566fcf129e6c4cd2ccbf9a
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-03-17 17:13:02 +0530

    Time: 1 ms (98.94%) | Memory: 43 MB (48.86%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.java
+++ 1127-last-stone-weight/last-stone-weight.java
@@ -1,23 +1,43 @@
+// class Solution {
+//     public int lastStoneWeight(int[] stones) {
+//         int n= stones.length;
+//         PriorityQueue<Integer> pq= new PriorityQueue<>(Collections.reverseOrder());
+
+//         for(int i=0;i<n;i++){
+//             pq.add(stones[i]);
+//         }
+
+//             while(pq.size()>1){
+//                 int weight= pq.poll();
+
+//                 weight-= pq.poll();
+
+//                 if(weight>0){
+//                     pq.add(weight);
+//                 }
+//             }
+
+//         return pq.isEmpty()?0 : pq.peek();
+//     }
+// }
+
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        int n= stones.length;
         PriorityQueue<Integer> pq= new PriorityQueue<>(Collections.reverseOrder());
 
-        for(int i=0;i<n;i++){
+        for(int i=0;i<stones.length;i++){
             pq.add(stones[i]);
         }
 
-            while(pq.size()>1){
-                int weight= pq.poll();
+        while(pq.size()>1){
+            int y= pq.poll();
+            int x= pq.poll();
 
-                weight-= pq.poll();
-
-                if(weight>0){
-                    pq.add(weight);
-                }
-
+            if(y!=x){
+                pq.add(y-x);
             }
+        }
 
         return pq.isEmpty()?0 : pq.peek();
-    }
-}
+
+    }}
