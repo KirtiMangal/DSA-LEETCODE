fde82a56a1c50fb092c248a9521e31236065918e
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-03-17 23:43:38 +0530

    Time: 45 ms (5.77%) | Memory: 47.8 MB (72.37%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,15 +1,39 @@
+// class Solution {
+//     public void moveZeroes(int[] nums) {
+//         int n= nums.length;
+//         int i=0;
+
+//         for(int j=0;j<n;j++){
+//             if(nums[j]!=0){
+//                 int temp= nums[i];
+//                 nums[i]= nums[j];
+//                 nums[j]= temp;
+
+//                 i++;
+//             }
+//         }
+//     }
+// }
+
 class Solution {
     public void moveZeroes(int[] nums) {
-        int n= nums.length;
-        int i=0;
+        int n = nums.length;
 
-        for(int j=0;j<n;j++){
-            if(nums[j]!=0){
-                int temp= nums[i];
-                nums[i]= nums[j];
-                nums[j]= temp;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == 0){
+                
+                // find next non-zero
+                int j = i + 1;
+                while(j < n && nums[j] == 0){
+                    j++;
+                }
 
-                i++;
+                // if found, swap
+                if(j < n){
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
             }
         }
     }
