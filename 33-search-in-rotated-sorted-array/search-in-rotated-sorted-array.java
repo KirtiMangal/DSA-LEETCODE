41077f3d928dda5eafa1660bf29ab0e2dc772979
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-03-21 18:49:04 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (89.29%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,37 +1,54 @@
 class Solution {
     public int search(int[] nums, int target) {
         int n= nums.length;
-        int low=0;
-        int high= n-1;
 
-        while(low<=high){
-            int mid= low+(high-low)/2;
+        int pivot = findPivot(nums);
 
-            if(nums[mid]==target){
-                return mid;
-            }
-
-            if(nums[low]<=nums[mid]){
-                if(nums[low]<=target && target<nums[mid]){
-                    high=mid-1;
-                }
-
-                else{
-                    low=mid+1;
-                }
-            }
-
-            else{
-                if(nums[mid]<target && target<=nums[high]){
-                    low=mid+1;
-                }
-
-                else{
-                    high=mid-1;
-                }
-            }
+        if(target>=nums[pivot] && target<=nums[n-1]){
+            return binarySearch(nums,pivot,n-1,target);
         }
 
-        return -1;
+        else{
+            return binarySearch(nums,0,pivot-1,target);
+        }
     }
-}
+
+        private int findPivot(int[] nums){
+            int n= nums.length;
+            int low=0;
+            int high= n-1;
+            while(low<high){
+                int mid= low+(high-low)/2;
+
+                if(nums[mid]>nums[high]){
+                    low=mid+1;
+                }
+
+                else{
+                    high=mid;
+                }
+            }
+
+            return low;
+        }
+
+        private int binarySearch(int[] nums, int low,int high, int target){
+            while(low<=high){
+                int mid= low+(high-low)/2;
+
+                if(nums[mid]==target){
+                    return mid;
+                }
+
+                else if(nums[mid]<target){
+                    low=mid+1;
+                }
+
+                else{
+                    high=mid-1;
+                }
+            }
+
+            return -1;
+        }
+    }
