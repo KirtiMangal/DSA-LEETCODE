f9eebc8e7f58e8c3a3222364a09dbca151542f66
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-03-24 12:43:23 +0530

    Time: 3 ms (61.16%) | Memory: 43.4 MB (81.23%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -1,25 +1,55 @@
+// class Solution {
+//     public String removeOuterParentheses(String s) {
+//         int n= s.length();
+//         Stack<Character> st= new Stack<>();
+//         StringBuilder sb= new StringBuilder();
+
+//         for(int i=0;i<n;i++){
+//             char ch= s.charAt(i);
+
+//             if(ch=='('){
+//                 if(!st.isEmpty())
+//                     sb.append(ch);
+//                     st.push(ch);
+//             }
+
+//                 else{
+//                     st.pop();
+//                     if(!st.isEmpty())
+//                         sb.append(ch);
+//                     }
+//                 }
+
+//                 return sb.toString();
+//             }
+//             }
+
+
 class Solution {
     public String removeOuterParentheses(String s) {
         int n= s.length();
-        Stack<Character> st= new Stack<>();
         StringBuilder sb= new StringBuilder();
+        int depth=0;
 
         for(int i=0;i<n;i++){
             char ch= s.charAt(i);
 
             if(ch=='('){
-                if(!st.isEmpty())
+                if(depth>0){
                     sb.append(ch);
-                    st.push(ch);
-            }
-
-                else{
-                    st.pop();
-                    if(!st.isEmpty())
-                        sb.append(ch);
-                    }
                 }
 
-                return sb.toString();
+                depth++;
             }
+
+            else{
+                depth--;
+                if(depth>0){
+                    sb.append(ch);
+                }
             }
+
+        }
+            return sb.toString();
+        }
+        }
