92191d0b8a0c8db77dd208e8404af4593df15618
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-03-24 11:01:57 +0530

    Time: 2 ms (99.60%) | Memory: 44.7 MB (58.25%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int n= s.length();
+        int m= t.length();
+        if(n!=m){
+            return false;
+        }
+
+        int[] freq= new int[26];
+        for(char ch : s.toCharArray()){
+            freq[ch-'a']++;
+        }
+
+        for(char ch : t.toCharArray()){
+            freq[ch-'a']--;
+        }
+
+        for(int i=0;i<26;i++){
+            if(freq[i]!=0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
