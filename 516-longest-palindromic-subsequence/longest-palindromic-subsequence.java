f22fbdd0339a87c6c016fc3cec6bd43e013b0613
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-04-29 19:15:00 +0530

    Time: 64 ms (14.46%) | Memory: 63.7 MB (60.24%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -8,15 +8,15 @@ class Solution {
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
                 if(s.charAt(i-1)==t.charAt(j-1)){
-                    dp[i][j]= dp[i-1][j-1] +1;
+                    dp[i][j]= dp[i-1][j-1]+1;
                 }
 
                 else{
                     dp[i][j]= Math.max(dp[i-1][j], dp[i][j-1]);
+                }
             }
         }
-    }
 
-    return dp[n][n];
+        return dp[n][n];
     }
 }
