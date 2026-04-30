99f3aaa06e445aed355a6c63b83bd05b6508cc17
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-04-30 08:34:02 +0530

    Time: 6 ms (93.24%) | Memory: 46.2 MB (82.41%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -8,7 +8,7 @@ class Solution {
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 if(word1.charAt(i-1)==word2.charAt(j-1)){
-                    dp[i][j]= dp[i-1][j-1]+1;
+                    dp[i][j]= dp[i-1][j-1] +1;
                 }
 
                 else{
