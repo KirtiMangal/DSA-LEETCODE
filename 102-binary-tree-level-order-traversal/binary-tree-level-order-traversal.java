462fc74ef2efef2c8da82e9c48bea07166ced00c
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-05-03 10:55:12 +0530

    Time: 2 ms (19.48%) | Memory: 46.8 MB (40.67%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -1,30 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> result= new ArrayList<>();
-        if(root==null)
-        return result;
+        if(root==null){
+            return result;
+        }
 
         Queue<TreeNode> q= new LinkedList<>();
         q.add(root);
+
         while(!q.isEmpty()){
-            int levelSize= q.size();
+            int size= q.size();
 
-            List<Integer> currentlevel= new ArrayList<>();
-            for(int i=0;i<levelSize;i++){
-                TreeNode current= q.poll();
-                currentlevel.add(current.val);
+            List<Integer> currentLevel= new ArrayList<>();
+            for(int i=0;i<size;i++){
+                TreeNode curr= q.poll();
+                currentLevel.add(curr.val);
 
-                if(current.left!=null){
-                    q.add(current.left);
+                if(curr.left!=null){
+                    q.add(curr.left);
                 }
 
-                if(current.right!=null){
-                    q.add(current.right);
+                if(curr.right!=null){
+                    q.add(curr.right);
                 }
-         
-            } 
+            }
 
-            result.add(currentlevel);
+            result.add(currentLevel);
         }
 
         return result;
