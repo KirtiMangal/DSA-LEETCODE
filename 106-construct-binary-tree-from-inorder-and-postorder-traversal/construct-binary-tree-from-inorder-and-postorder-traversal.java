7097711f2dfafd7d57cf06813bf66b0af688532f
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-05-05 19:19:08 +0530

    Time: 1 ms (98.15%) | Memory: 46.4 MB (47.66%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -17,30 +17,32 @@ class Solution {
     int postIndex;
     public TreeNode buildTree(int[] inorder, int[] postorder) {
         postIndex= postorder.length-1;
+        int n= inorder.length;
 
         Map<Integer,Integer> map= new HashMap<>();
 
-        for(int i=0;i<inorder.length;i++){
+        for(int i=0;i<n;i++){
             map.put(inorder[i],i);
         }
 
         return helper(postorder,0,inorder.length-1,map);
     }
 
-        
-        public TreeNode helper(int[] postorder,int inStart,int inEnd, Map<Integer,Integer> map){
-            if(inStart>inEnd){
-                return null;
-            }
-
-            int rootVal= postorder[postIndex--];
-            TreeNode root= new TreeNode(rootVal);
-
-            int index= map.get(rootVal);
-
-            root.right= helper(postorder,index+1,inEnd,map);
-            root.left= helper(postorder,inStart,index-1,map);
-
-            return root;
+    public TreeNode helper(int[] postorder, int inStart, int inEnd, Map<Integer,Integer> map){
+        if(inStart>inEnd){
+            return null;
         }
+
+        int rootVal= postorder[postIndex--];
+        TreeNode root= new TreeNode(rootVal);
+
+        int index= map.get(rootVal);
+
+        root.right= helper(postorder,index+1,inEnd,map);
+        root.left= helper(postorder,inStart,index-1,map);
+
+        return root;
+
+
     }
+}
