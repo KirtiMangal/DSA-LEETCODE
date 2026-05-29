710038df491592a5c3e15268e301980446d82cf7
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-05-29 17:09:46 +0530

    Time: 25 ms (18.67%), Space: 50.7 MB (85.19%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,18 +1,38 @@
+// class Solution {
+//     public int kthSmallest(int[][] matrix, int k) {
+//         int m= matrix.length;
+//         int n= matrix[0].length;
+
+//         ArrayList<Integer> list= new ArrayList<>();
+
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 list.add(matrix[i][j]);
+//             }
+//         }
+
+//         Collections.sort(list);
+
+//         return list.get(k-1);
+//     }
+// }
+
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
         int m= matrix.length;
         int n= matrix[0].length;
 
-        ArrayList<Integer> list= new ArrayList<>();
+        PriorityQueue<Integer> pq= new PriorityQueue<>();
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                list.add(matrix[i][j]);
+                pq.add(matrix[i][j]);
             }
         }
 
-        Collections.sort(list);
+        for(int i=1;i<k;i++){
+            pq.poll();
+        }
 
-        return list.get(k-1);
-    }
-}
+        return pq.peek();
+    }}
