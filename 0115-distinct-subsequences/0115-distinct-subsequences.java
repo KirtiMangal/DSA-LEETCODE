a05667478153b8accd9cac91725d944251db7bb8
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-06-24 22:55:46 +0530

    Time: 19 ms (72.36%), Space: 54.2 MB (73.75%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -9,9 +9,13 @@ class Solution {
             dp[i][0]=1;
         }
 
+        for(int j=1;j<=m;j++){
+            dp[0][j]=0;
+        }
+
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(s.charAt(i-1)== t.charAt(j-1)){
+                if(s.charAt(i-1)==t.charAt(j-1)){
                     dp[i][j]= dp[i-1][j-1] + dp[i-1][j];
                 }
 
@@ -20,6 +24,7 @@ class Solution {
                 }
             }
         }
+
         return dp[n][m];
     }
 }
