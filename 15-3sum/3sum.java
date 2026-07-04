7fe9b9efb7e7c207626524105a556e9d0e1d76fb
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-07-05 01:12:20 +0530

    Time: 889 ms (8.52%) | Memory: 60.4 MB (9.90%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,35 +1,56 @@
+// class Solution {
+//     public List<List<Integer>> threeSum(int[] nums) {
+//         int n= nums.length;
+
+//         Set<List<Integer>> set= new HashSet<>();
+
+//         for(int i=0;i<n;i++){
+//             for(int j=i+1;j<n;j++){
+//                 for(int k=j+1;k<n;k++){
+//                     if(nums[i]+nums[j]+nums[k]==0){
+//                         List<Integer> temp= Arrays.asList(nums[i],nums[j],nums[k]);
+
+//                         Collections.sort(temp);
+//                         set.add(temp);
+//                     }
+//                 }
+//             }
+//         }
+
+//         return new ArrayList<>(set);
+//     }
+// }
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         int n= nums.length;
-
         Arrays.sort(nums);
-        Set<List<Integer>> result= new HashSet<>();
+
+        Set<List<Integer>> set= new HashSet<>();
 
         for(int i=0;i<n;i++){
-        int left=i+1;
-        int right= n-1;
+            int j= i+1;
+            int k= n-1;
 
-        while(left<right){
-            int sum= nums[i]+ nums[left]+ nums[right];
+            while(j<k){
+                int sum= nums[i]+nums[j]+nums[k];
 
-            if(sum==0){
-                result.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                if(sum==0){
+                    set.add(Arrays.asList(nums[i],nums[j],nums[k]));
 
-                left++;
-                right--;
-            }
+                    j++;
+                    k--;
+                }
 
-            else if(sum<0){
-                left++;
-            }
+                else if(sum<0){
+                    j++;
+                }
 
-            else{
-                right--;
+                else{
+                    k--;
+                }
             }
         }
 
-    }
-
-    return new ArrayList<>(result);
-    }
-}
+        return new ArrayList<>(set);
+    }}
