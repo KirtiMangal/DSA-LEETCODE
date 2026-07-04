4a7d77d12d81ffa79592a6cc7c788f5b12998535
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-07-04 22:46:47 +0530

    Time: 39 ms (35.57%) | Memory: 46.7 MB (92.94%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -4,8 +4,7 @@ class Solution {
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 if(nums[i]+nums[j]==target){
-                    int[] a={i,j};
-
+                    int[] a= {i,j};
                     return a;
                 }
             }
