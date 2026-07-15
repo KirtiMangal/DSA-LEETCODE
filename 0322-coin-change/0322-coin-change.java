1a5ca53e63979736b9d6400fb3e141e3d79e0a37
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-07-16 00:24:06 +0530

    Time: 28 ms (23.95%), Space: 47.3 MB (13.43%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,25 +1,43 @@
 class Solution {
+
     public int coinChange(int[] coins, int amount) {
-        int INF = amount + 1;
-        int[] dp = new int[amount + 1];
 
-        Arrays.fill(dp, INF);
-        dp[0] = 0;
+        int n = coins.length;
+        int[][] dp = new int[n][amount + 1];
 
-        for (int i = 1; i <= amount; i++) {
-            for (int coin : coins) {
-                if (coin <= i) {
-                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
-                }
-            }
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], -1);
         }
 
-        if(dp[amount]>amount)
-        return -1;
+        int ans = solve(n - 1, amount, coins, dp);
 
-        else
-        return dp[amount];
+        return ans >= (int)1e9 ? -1 : ans;
+    }
 
-        //return dp[amount] > amount ? -1 : dp[amount];
+    public int solve(int ind, int target, int[] coins, int[][] dp) {
+
+        // Base Case
+        if (ind == 0) {
+
+            if (target % coins[0] == 0)
+                return target / coins[0];
+
+            return (int)1e9;
+        }
+
+        if (dp[ind][target] != -1)
+            return dp[ind][target];
+
+        // Don't take current coin
+        int notTake = solve(ind - 1, target, coins, dp);
+
+        // Take current coin
+        int take = (int)1e9;
+
+        if (coins[ind] <= target) {
+            take = 1 + solve(ind, target - coins[ind], coins, dp);
+        }
+
+        return dp[ind][target] = Math.min(take, notTake);
     }
 }
