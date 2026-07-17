ff995cf65bee18443d50be2fb7df2f6b06e3a109
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-07-17 11:08:38 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (56.83%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -14,17 +14,44 @@
 //     }
 // }
 
+// class Solution {
+//     public int rob(int[] nums) {
+//         int n= nums.length;
+//         int[] dp= new int[n+2];
+
+//         for(int i=n-1;i>=0;i--){
+//             int take= nums[i]+ dp[i+2];
+//             int nottake= dp[i+1];
+
+//             dp[i]= Math.max(take,nottake);
+//         }
+
+//         return dp[0];
+//     }}
+
 class Solution {
     public int rob(int[] nums) {
         int n= nums.length;
-        int[] dp= new int[n+2];
+        Integer[] dp= new Integer[n];
+       return solve(nums,0,dp);
+    }
 
-        for(int i=n-1;i>=0;i--){
-            int take= nums[i]+ dp[i+2];
-            int nottake= dp[i+1];
+
+        private int solve(int[] nums, int i, Integer[] dp){
+            int n= nums.length;
+            if(i>=n){
+                return 0;
+            }
+
+            if(dp[i]!=null){
+                return dp[i];
+            }
+
+            int take= nums[i] + solve(nums,i+2,dp);
+            int nottake= solve(nums,i+1,dp);
 
             dp[i]= Math.max(take,nottake);
-        }
 
-        return dp[0];
-    }}
+            return dp[i];
+        }
+}
