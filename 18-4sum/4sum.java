316bd0abe502422f5dc8ff48105dd2a2e0098cef
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-07-19 23:10:38 +0530

    Time: 148 ms (8.44%) | Memory: 47.2 MB (12.26%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,43 +1,17 @@
-// class Solution {
-//     public List<List<Integer>> fourSum(int[] nums, int target) {
-//         int n= nums.length;
-
-//         Set<List<Integer>> set= new HashSet<>();
-
-//         for(int i=0;i<n;i++){
-//             for(int j=i+1;j<n;j++){
-//                 for(int k=j+1;j<n;k++){
-//                     for(int l=k+1;l<n;l++){
-//                         long sum= (long) nums[i]+nums[j]+nums[k]+nums[l];
-//                         if(sum==target){
-//                             List<Integer> temp= Arrays.asList(nums[i],nums[j],nums[k],nums[l]);
-
-//                             Collections.sort(temp);
-//                             set.add(temp);
-//                         }
-//                     }
-//                 }
-//             }
-//         }
-
-//         return new ArrayList<>(set);
-
-//     }
-// }
-
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         int n= nums.length;
         Set<List<Integer>> set= new HashSet<>();
+        
         Arrays.sort(nums);
-
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 int k= j+1;
                 int l= n-1;
 
                 while(k<l){
-                    long sum= (long) nums[i]+nums[j]+nums[k]+nums[l];
+                    long sum= (long)nums[i]+nums[j]+nums[k]+nums[l];
+
                     if(sum==target){
                         set.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
 
@@ -57,4 +31,5 @@ class Solution {
         }
 
         return new ArrayList<>(set);
-    }}
+    }
+}
