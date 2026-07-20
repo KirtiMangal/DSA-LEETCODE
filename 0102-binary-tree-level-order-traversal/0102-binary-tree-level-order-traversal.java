f4f1ba1940f6de1b39d43b49af1f0d16737dd11e
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-07-20 14:06:00 +0530

    Time: 1 ms (96.23%), Space: 46.4 MB (96.04%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -1,32 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> result= new ArrayList<>();
-        if(root==null)
-        return result;
+        List<List<Integer>> list= new ArrayList<>();
+        if(root==null){
+            return list;
+        }
 
         Queue<TreeNode> q= new LinkedList<>();
         q.add(root);
+
         while(!q.isEmpty()){
-            int levelSize= q.size();
+            int size= q.size();
 
-            List<Integer> currentlevel= new ArrayList<>();
-            for(int i=0;i<levelSize;i++){
-                TreeNode current= q.poll();
-                currentlevel.add(current.val);
+            List<Integer> current= new ArrayList<>();
 
-                if(current.left!=null){
-                    q.add(current.left);
-                }
+            for(int i=0;i<size;i++){
+            TreeNode curr= q.poll();
 
-                if(current.right!=null){
-                    q.add(current.right);
-                }
-         
-            } 
+            current.add(curr.val);
 
-            result.add(currentlevel);
+            if(curr.left!=null){
+                q.add(curr.left);
+            }
+
+            if(curr.right!=null){
+                q.add(curr.right);
+            }
         }
 
-        return result;
+        list.add(current);
+    }
+
+    return list;
     }
 }
