33fa81be56c1f78526f451db7aa0cc8d0f46300a
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-07-29 15:12:08 +0530

    Time: 2 ms (88.85%) | Memory: 48 MB (43.08%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,14 +1,29 @@
+// class Solution {
+//     public boolean canJump(int[] nums) {
+//         int n= nums.length;
+//         int finaltime=n-1;
+
+//         for(int i=n-2;i>=0;i--){
+//             if(i+nums[i]>=finaltime){
+//                 finaltime=i;
+//             }
+//         }
+
+//         return finaltime==0;
+//     }
+// }
+
 class Solution {
     public boolean canJump(int[] nums) {
         int n= nums.length;
-        int finaltime=n-1;
-
-        for(int i=n-2;i>=0;i--){
-            if(i+nums[i]>=finaltime){
-                finaltime=i;
+        int max=0;
+        for(int i=0;i<n;i++){
+            if(i>max){
+                return false;
             }
+
+            max= Math.max(max, i+nums[i]);
         }
 
-        return finaltime==0;
-    }
-}
+        return true;
+    }}
