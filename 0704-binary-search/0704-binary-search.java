b65564d875d12d68a70be1aeca2251c74a204ed4
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-07-31 13:24:05 +0530

    Time: 0 ms (100%), Space: 48.5 MB (14.31%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -12,12 +12,14 @@ class Solution {
             }
 
             else if(nums[mid]<=target){
-                low= mid+1;
+                low=mid+1;
             }
 
             else{
                 high=mid-1;
             }
+
+
         }
 
         return -1;
