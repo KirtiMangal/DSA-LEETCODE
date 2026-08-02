c8c714be04b5b4bf178f24f7f927b720950e9d3d
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-08-02 12:32:42 +0530

    Time: 8 ms (91.49%) | Memory: 49.2 MB (43.55%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -3,25 +3,23 @@ class Solution {
         int n= intervals.length;
         Arrays.sort(intervals,(a,b)->a[0]-b[0]);
 
-        List<int[]> result= new ArrayList<>();
-        int[] curr= intervals[0];
+        List<int[]> list= new ArrayList<>();
+        int curr[]= intervals[0];
 
-        for(int i=1;i<n;i++){
-            int[] next= intervals[i];
+        for(int i=0;i<n;i++){
+            int next[]= intervals[i];
 
             if(next[0]<=curr[1]){
                 curr[1]= Math.max(next[1],curr[1]);
             }
 
             else{
-
-            result.add(curr);
-            curr=next;
+                list.add(curr);
+                curr= next;
             }
         }
 
-        result.add(curr);
-        return result.toArray(new int[result.size()][]);
-
+        list.add(curr);
+        return list.toArray(new int[list.size()][]);
     }
 }
