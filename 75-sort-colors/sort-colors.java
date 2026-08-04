4a666f422ae2db3597e16921b6cde7855a0b5036
KirtiMangal2006 <156676505+KirtiMangal@users.noreply.github.com>
2026-08-05 02:08:07 +0530

    Time: 3 ms (14.55%) | Memory: 43.6 MB (43.96%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void sortColors(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+        
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            // If no elements were swapped in the inner loop, array is sorted
+            if (!swapped) break;
+        }
+    }
+}
